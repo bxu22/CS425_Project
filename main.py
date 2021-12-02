bf83e213d8a94aab7ef7b1da539bc43d296d17d8
bxu22 <bxu22@hawk.iit.edu>
2021-12-02 09:10:48 -0600

    Fixed most of the SQL stuff for creating tables

--- main.py
+++ main.py
@@ -46,179 +46,184 @@ def read_query(connection, query):
 #drop table Organ_Donor;
 create_Organ_Donor_table="""
 create table Organ_Donor (
-name VARCHAR(100) not null,
-blood_type VARCHAR(100),
-age integer,
+name VARCHAR(100) NOT NULL,
+blood_type VARCHAR(10) NOT NULL,
+age INTEGER,
 chronic_diseases VARCHAR(100),
-drug_usage boolean,
-last_tattoo_date date,
-medication_history varchar(100),
-last_donation_time date,
+drug_usage BOOLEAN,
+last_tattoo_date DATE,
+medication_history VARCHAR(100),
+last_donation_time DATE,
 phone_or_email VARCHAR(100),
 city VARCHAR(100),
 state VARCHAR(100),
-organ_name VARCHAR(100) not null,
-primary key (name, organ_name) ---on delete cascade how?
+organ_name VARCHAR(100) NOT NULL,
+PRIMARY KEY (name, organ_name)
 );
 """
 #drop table Blood_Donor;
 create_Blood_Donor_table="""
-create table Blood_Donor(
-name VARCHAR(100) not null,
-blood_type VARCHAR(100) not null,
-age integer,
+create table Blood_Donor (
+name VARCHAR(100) NOT NULL,
+blood_type VARCHAR(10) NOT NULL,
+age INTEGER,
 chronic_diseases VARCHAR(100),
-drug_usage boolean,
-last_tattoo_date date,
-medication_history varchar(100),
-last_donation_time date,
+drug_usage BOOLEAN,
+last_tattoo_date DATE,
+medication_history VARCHAR(100),
+last_donation_time DATE,
 phone_or_email VARCHAR(100),
 city VARCHAR(100),
 state VARCHAR(100),
-primary key (name, blood_type)
+PRIMARY KEY (name, blood_type)
 );
 """
 
 #drop table Patient;
 create_Patient_table="""
 create table Patient (
-name VARCHAR(100) not null,
-blood_type VARCHAR(100),
-age integer,
+id SERIAL NOT NULL,
+name VARCHAR(100) NOT NULL,
+blood_type VARCHAR(10) NOT NULL,
+age INTEGER,
 need_organ VARCHAR(100),
-need_blood VARCHAR(100),
-pays integer,
-primary key (name)
+need_blood VARCHAR(10),
+pays INTEGER,
+PRIMARY KEY (id)
 );
 """
 
-#drop table organ;
+#drop table Organ;
 create_Organ_table="""
-create table organ(
-organ_name VARCHAR(100) not null,
-donor_name VARCHAR(100) not null,
-recipient VARCHAR(100),
-life integer,
-availability_date date,
-primary key (organ_name, donor_name),
-foreign key (organ_name, donor_name) references Organ_donor on delete cascade,
-foreign key (recipient) references Patient
+create table Organ (
+donor_name VARCHAR(100) NOT NULL,
+organ_name VARCHAR(100) NOT NULL,
+recipient INTEGER,
+life INTEGER,
+availability_date DATE,
+PRIMARY KEY (donor_name, organ_name),
+FOREIGN KEY (donor_name, organ_name) REFERENCES Organ_Donor(name, organ_name) ON DELETE CASCADE,
+FOREIGN KEY (recipient) REFERENCES Patient(id)
 );
 """
 
 #drop table Hospital;
 create_Hospital_table="""
 create table Hospital (
-name VARCHAR(100) not null,
-city VARCHAR(100),
-state VARCHAR(100),
-hospitalization_cost integer,
-primary key (name)
+id SERIAL NOT NULL,
+name VARCHAR(100) NOT NULL,
+city VARCHAR(100) NOT NULL,
+state VARCHAR(100) NOT NULL,
+hospitalization_cost INTEGER,
+PRIMARY KEY (id)
 );
 """
 
 #drop table Doctor;
 create_Doctor_table="""
 create table Doctor (
-name VARCHAR(100) not null,
+id SERIAL NOT NULL,
+name VARCHAR(100) NOT NULL,
 specialization VARCHAR(100),
-fee integer,
-primary key (name)
+fee INTEGER,
+PRIMARY KEY (id)
 );
 """
 
-#drop table donor_application;
+#drop table Donor_Application;
 create_Donor_Application_table="""
 create table Donor_Application (
-donorID integer not null,
-blood_type VARCHAR(100),
-age integer,
+id SERIAL NOT NULL,
+blood_type VARCHAR(10),
+age INTEGER,
 chronic_diseases VARCHAR(100),
 drug_usage VARCHAR(100),
-last_tattoo_date date,
+last_tattoo_date DATE,
 medication_history VARCHAR(100),
-last_donation_time date,
+last_donation_time DATE,
 phone_or_email VARCHAR(100),
 city VARCHAR(100),
 state VARCHAR(100),
-primary key (donorID)
+PRIMARY KEY (id)
 );
 """
-#drop table works_at;
+#drop table Works_At;
 create_Works_At_table="""
-create table works_at (
-hospital_name VARCHAR (100) not null,
-doctor_name VARCHAR (100) not null,
-primary key (doctor_name, hospital_name),
-foreign key (doctor_name) references Doctor(name) on delete cascade,
-foreign key (hospital_name) references Hospital(name) on delete cascade
+create table Works_At (
+doctor_id INTEGER NOT NULL,
+hospital_id INTEGER NOT NULL,
+PRIMARY KEY (doctor_id, hospital_id),
+FOREIGN KEY (doctor_id) REFERENCES Doctor(id) ON DELETE CASCADE,
+FOREIGN KEY (hospital_id) REFERENCES Hospital(id) ON DELETE CASCADE
 );
 """
 
-#drop table patient_need;
+#drop table Patient_Need;
 create_Patient_Need_table="""
-create table patient_need (
-patient_ID VARCHAR(100) not null,
-organ_name varchar(100),
-organ_donorr varchar(100),
-blood_type varchar(100),
-blood_donorr varchar(100),
-primary key (patient_ID),
-foreign key (patient_ID) references Patient(name) on delete cascade,
-foreign key (organ_donorr, organ_name) references Organ_Donor(name, organ_name) ,
-foreign key (blood_donorr, blood_type) references Blood_Donor(name, blood_type)
+create table Patient_Need (
+patient_ID INTEGER NOT NULL,    
+organ_name VARCHAR(100),
+organ_donorr VARCHAR(100),
+blood_type VARCHAR(10),
+blood_donorr VARCHAR(100),
+PRIMARY KEY (patient_ID),
+FOREIGN KEY (patient_ID) REFERENCES Patient(id) ON DELETE CASCADE,
+FOREIGN KEY (organ_donorr, organ_name) REFERENCES Organ_Donor(name, organ_name),
+FOREIGN KEY (blood_donorr, blood_type) REFERENCES Blood_Donor(name, blood_type)
 );
 """
 
-#drop table operates;
+#drop table Operates;
 create_Operates_table="""
-create table operates (
-Doctor_name VARCHAR (100) not null,
-Patient_name VARCHAR(100) not null,
-Primary key (doctor_name, patient_name),
-foreign key (doctor_name) references doctor(name) on delete cascade,
-foreign key (patient_name) references patient(name) on delete cascade
+create table Operates (
+doctor_id INTEGER NOT NULL,
+patient_id INTEGER NOT NULL,
+PRIMARY KEY (doctor_id, patient_id),
+FOREIGN KEY (doctor_id) REFERENCES Doctor(id) ON DELETE CASCADE,
+FOREIGN KEY (patient_id) REFERENCES Patient(id) ON DELETE CASCADE
 );
 """
 
-#drop table pays;
+#drop table Pays;
 create_Pays_table="""
-create table pays (
-patient_name VARCHAR(100) not null,
-hospital_name varchar(100),
-primary key (patient_name),
-Foreign key (patient_name) references patient(name) on delete cascade,
-foreign key (hospital_name) references hospital(name) on delete cascade
+create table Pays (
+patient_id INTEGER NOT NULL,
+hospital_id INTEGER NOT NULL,
+PRIMARY KEY (patient_id),
+FOREIGN KEY (patient_id) REFERENCES Patient(id) ON DELETE CASCADE,
+FOREIGN KEY (hospital_id) REFERENCES Hospital(id) ON DELETE CASCADE
 );
 """
 
-#drop table in_;
+#drop table In_;
 create_In_table="""
 create table in_ (
-doctor_name VARCHAR(100),
-patient_name VARCHAR(100) not null,
-hospital_name VARCHAR(100),
-primary key (hospital_name),
-foreign key (doctor_name) references doctor(name),
-foreign key (patient_name) references patient(name),
-foreign key (hospital_name) references hospital(name) on delete cascade
+doctor_id INTEGER,
+patient_id INTEGER,
+hospital_id INTEGER NOT NULL,
+PRIMARY KEY (hospital_id),
+PRIMARY KEY (doctor_id),
+PRIMARY KEY (patient_id),
+FOREIGN KEY (patient_id) REFERENCES Patient(id),
+FOREIGN KEY (doctor_id) REFERENCES Doctor(id),
+FOREIGN KEY (hospital_id) REFERENCES Hospital(id) ON DELETE CASCADE
 );
 """
 
-#drop table applies;
+#drop table Applies;
 create_Applies_table="""
 create table applies (
-blood_type VARCHAR(100),
-organ_name VARCHAR(100),
-donorID integer not null,
-hospital_name VARCHAR(100) not null,
-Primary key (DonorID, hospital_name),
-Foreign key (DonorID) references donor_application(donorID) on delete cascade,
-foreign key (hospital_name) references hospital(name) on delete cascade
+blood_type VARCHAR(10) NOT NULL,
+organ_name VARCHAR(100) NOT NULL,
+donor_id INTEGER NOT NULL,
+hospital_id INTEGER NOT NULL,
+PRIMARY KEY (donor_id, hospital_id),
+FOREIGN KEY (donor_id) REFERENCES Donor_Application(id) ON DELETE CASCADE,
+FOREIGN KEY (hospital_id) REFERENCES Hospital(id) ON DELETE CASCADE
 );
 """
 
-#CHANGE THIS
+# BEING CHANGED AS QUERIES
 #drop table Blood_donor_list;
 #Should be search
 # create_Blood_Donor_List_table="""
@@ -231,7 +236,7 @@ foreign key (hospital_name) references hospital(name) on delete cascade
 # );
 # """
 
-#CHANGE THIS
+# BEING CHANGED AS QUERIES
 #drop table organ_donor_list;
 #Should be search
 # create_Organ_Donor_List_table="""
@@ -269,10 +274,10 @@ create index Organ_Donor_List_index on Organ_Donor_List(ID);
 #Should be search
 create_All_Donors_table="""
 create table all_donors (
-blood_donor_ID VARCHAR(100),
-organ_donor_ID VARCHAR(100),
-foreign key (blood_donor_ID) references Blood_Donor_List,
-foreign key (organ_donor_ID) references Organ_Donor_List
+blood_donor_id VARCHAR(100),
+organ_donor_id VARCHAR(100),
+foreign key (blood_donor_id) references Blood_Donor_List(id),
+foreign key (organ_donor_id) references Organ_Donor_List(id)
 );
 """
 create_Admin_role="""
@@ -282,16 +287,13 @@ GRANT ALL PRIVILEDGE on * TO admin;
 create_Doctor_User_role="""
 CREATE ROLE doctor_user;
 GRANT ALL PRIVILEDGE on Patient TO doctor_user;
-GRANT ALL PRIVILEDGE on Organ_donor TO doctor_user;
+GRANT ALL PRIVILEDGE on Organ_Donor TO doctor_user;
 """
 create_Patient_User_role="""
 CREATE ROLE patient_user;
 GRANT INSERT PRIVILEDGE on Donor_Application TO patient_user;
 """
 
-#create type Blood_Donor_List as table Donor_List [not null];
-#create type Organ_Donor_List is table of Donor_List [not null];
-#create role doctorUser;
 #grant doctorUser TO Patient ;
 #grant doctorUser to Organ_donor;
 
@@ -301,83 +303,83 @@ GRANT INSERT PRIVILEDGE on Donor_Application TO patient_user;
 
 
 while(True):
-    print("Please login to the database.")
-    host_n= input("Enter your host name: ")
-    db_n= input("Enter your database name: ")
-    user_n=input("Enter your user name: ") #admin, or other users.
-    user_p = input("Enter your user password: ")
+    print('Please login to the database.')
+    host_n= input('Enter your host name: ')
+    db_n= input('Enter your database name: ')
+    user_n=input('Enter your user name: ') #admin, or other users.
+    user_p = input('Enter your user password: ')
 
     try:
         connection = create_server_connection(host_n, db_n, user_n, user_p)
-        print("Login successfull")
+        print('Login successful')
         #will deal with user types later (maybe in try-excepts)
         while(True):
-            print("What do you want to do? \n1: Add data to tables, 2: View tables & lists, 3: View reports, 4: Setup the database (only once) ")
+            print('What do you want to do? \n1: Add data to tables, 2: View tables & lists, 3: View reports, 4: Setup the database (only once)')
             main_ans = input()
 
             #add to a table
-            if main_ans = 1:
-                print("Tables: Donor, Organ")
-                table_name = input("Which table do you want to add to: ")
-                data_add = input("Enter the data: ")
-                if (table_name == "Donor" or table_name=="Organ"):
-                    data_add_q="INSERT INTO "+table_name+" VALUES "+data_add
-                    try:
-                        execute_query(connection, data_add_string)
-                        print("added data to table")
+            if main_ans == 1:
+                print('Tables: Donor, Organ')
+                table_name = input('Which table do you want to add to: ')
+                data_add = input('Enter the data: ')
+                if (table_name == 'Donor' or table_name == 'Organ'):
+                    data_add_q = 'INSERT INTO ' + table_name + ' VALUES ' + data_add        # I think this needs to be in two different if statements
+                    try:                                                                    # since donor and organ have different attributes, also you have to specify the parameters
+                        execute_query(connection, data_add_q)
+                        print('added data to table')
                     except Error as err: #wrong data, or user cannot do this action.
                         print(f"Error: '{err}'")
                 else:
-                    print("Tried to reach a wrong table, go back to main menu")
+                    print('Tried to reach a wrong table, go back to main menu')
 
             #view tables & lists
-            elif main_ans = 2: # (!)update queries related to view like blood_donor_list
-                view_q="" #this is the query that is going to be run after the if-conditions.
-                print("View: Donor, Organ, Patient, Blood_Donor_List, Organ_Donor_List, Donor_Match_List")
-                view_name = input("Which view do you want: ")
+            elif main_ans == 2: # (!)update queries related to view like blood_donor_list
+                view_q = '' #this is the query that is going to be run after the if-conditions.
+                print('View: Donor, Organ, Patient, Blood_Donor_List, Organ_Donor_List, Donor_Match_List')
+                view_name = input('Which view do you want: ')
 
-                if(view_name=="Donor" or view_name=="Organ" or view_name=="Patient"):
-                    view_q = "SELECT * FROM "+view_name
+                if(view_name == 'Donor' or view_name == 'Organ' or view_name == 'Patient'):
+                    view_q = 'SELECT * FROM ' + view_name
 
-                elif(view_name=="Organ_Donor_List"):
-                    do_search = input("Do you want to (1) view all or (2) search by sth:")
-                    if (do_search==1): #view all table
-                        view_q = "SELECT * FROM "+view_name
+                elif(view_name == 'Organ_Donor_List'):
+                    do_search = input('Do you want to (1) view all or (2) search by sth:')
+                    if (do_search == 1): #view all table
+                        view_q = 'SELECT * FROM '+view_name
 
-                    elif (do_search==2): #search by sth
-                        search_by = input("Search by region, organ, doctor")
-                        search_val = input("Search value: ")
+                    elif (do_search == 2): #search by sth
+                        search_by = input('Search by region, organ, doctor')
+                        search_val = input('Search value: ')
                         #write the query here
-                        view_q = "SELECT * FROM Organ_Donor WHERE "+ search_by+"="+search_val
+                        view_q = 'SELECT * FROM Organ_Donor WHERE ' + search_by + '=' + search_val
 
-                elif(view_name=="Blood_Donor_List"):
-                    do_search = input("Do you want to (1) view all or (2) search by sth:")
-                    if (do_search==1): #view all table
-                        view_q = "SELECT * FROM "+view_name
+                elif(view_name == 'Blood_Donor_List'):
+                    do_search = input('Do you want to (1) view all or (2) search by sth:')
+                    if (do_search == 1): #view all table
+                        view_q = 'SELECT * FROM ' + view_name
 
-                    elif (do_search==2): #search by sth
-                        search_by = input("Search by region, blood_type, typeAvailability, age_group")
-                        search_val = input("Search value: ")
+                    elif (do_search == 2): #search by sth
+                        search_by = input('Search by region, blood_type, typeAvailability, age_group')
+                        search_val = input('Search value: ')
                         #write the query here
-                        view_q = "SELECT * FROM Organ_Donor WHERE "+ search_by+"="+search_val
+                        view_q = 'SELECT * FROM Organ_Donor WHERE ' + search_by + '=' + search_val
 
-                elif(view_name=="Donor_Match_List"): #look further into
-                    organ_or_blood = input("Do you want to look at (1) Organ matches or (2) Blood matches: ")
-                    if(organ_or_blood==1):
-                        do_search = input("Do you want to (1) view all or (2) search by organ:")
-                        if (do_search==1): #view all table
+                elif(view_name == 'Donor_Match_List'): #look further into
+                    organ_or_blood = input('Do you want to look at (1) Organ matches or (2) Blood matches: ')
+                    if(organ_or_blood == 1):
+                        do_search = input('Do you want to (1) view all or (2) search by organ:')
+                        if (do_search == 1): #view all table
                             view_q = Organ_Match_List
-                        elif (do_search==2):
-                            organ_n = input("which organ do you want to search for?: ")
-                            view_q = Organ_Match_List+" WHERE Organ_Donor.organ_name="+organ_n
+                        elif (do_search == 2):
+                            organ_n = input('Which organ do you want to search for?: ')
+                            view_q = Organ_Match_List + ' WHERE Organ_Donor.organ_name=' + organ_n
 
-                    elif(organ_or_blood==2):
-                        do_search = input("Do you want to (1) view all or (2) search by blood:")
-                        if (do_search==1): #view all table
+                    elif(organ_or_blood == 2):
+                        do_search = input('Do you want to (1) view all or (2) search by blood:')
+                        if (do_search == 1): #view all table
                             view_q = Blood_Match_List
-                        elif (do_search==2):
-                            blood_type_n = input("which blood type do you want to search for?: ")
-                            view_q = Blood_Match_List+" WHERE Blood_Donor.blood_type="+blood_type_n
+                        elif (do_search == 2):
+                            blood_type_n = input('Which blood type do you want to search for?: ')
+                            view_q = Blood_Match_List + ' WHERE Blood_Donor.blood_type=' + blood_type_n
 
                 #view_q is set according to the request.
                 try:
@@ -388,10 +390,10 @@ while(True):
                     print(f"Error: '{err}'")
 
             #view reports
-            elif main_ans = 3:
+            elif main_ans == 3:
                 pass
             #create the database, should be run only once.
-            elif main_ans = 4:
+            elif main_ans == 4:
                 try:
                     execute_query(connection, create_Organ_Donor_table)
                     execute_query(connection, create_Blood_Donor_table)
@@ -410,10 +412,11 @@ while(True):
                     execute_query(connection, create_Admin_role)
                     execute_query(connection, create_Doctor_User_role)
                     execute_query(connection, create_Patient_User_role)
+                    
                     #ADD DUMMY DATA TOO
                 except Error as err:
                     print(f"Error: '{err}'")
             else:
-                print("Invalid request. input 1, 2, 3 or 4.") #return back to question
+                print('Invalid request. input 1, 2, 3 or 4.') #return back to question
     except Error as err:
         print(f"Error: '{err}'")
